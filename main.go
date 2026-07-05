863a1f606498c137e26f7472557f71dbe577496e
Reda Hilal <reda0x04@gmail.com>
2026-07-05 08:52:11 +0400

    practice slices

--- main.go
+++ main.go
@@ -4,10 +4,25 @@ import "fmt"
 
 func main() {
 
-	nums := []int{1, 2, 3, 4, 5}
+	nums := [5]int{1, 2, 3, 4, 5}
+	part := nums[1:3] // slice from index 1 to 3 (4 is exclusive)
 	fmt.Println("Numbers:", nums)
+	fmt.Println("Part:", part)
 
-	startCLI()
+	part = append(part, 6, 7, 8, 9) // append 6 to the slice
+	fmt.Println("After appending 6:")
+	fmt.Println("Numbers:", nums)
+	fmt.Println("Part:", part)
+
+	// startCLI()
+}
+
+func sum(nums [5]int) int {
+	total := 0
+	for _, n := range nums {
+		total += n
+	}
+	return total
 }
 
 func startCLI() {
