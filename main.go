0ec37fb1389aa1e8082c513824db2d5e4aae9b28
Reda Hilal <reda0x04@gmail.com>
2026-07-06 14:15:04 +0400

    strings and runes testing

--- main.go
+++ main.go
@@ -1,9 +1,27 @@
 package main
 
 import (
-	"go-fundamentals/internal/cli"
+	"fmt"
+	"unicode/utf8"
 )
 
 func main() {
-	cli.Start()
+
+	const s = "السلام عليكم 😊👍"
+
+	fmt.Println(s)
+
+	fmt.Println("Bytes:")
+	for i := range len(s) {
+		fmt.Printf("%x ", s[i])
+	}
+
+	fmt.Println("\nlength:", len(s))
+	fmt.Println("Rune count:", utf8.RuneCountInString(s))
+
+	for idx, runeValue := range s {
+		fmt.Printf("%#U starts at %d\n", runeValue, idx)
+	}
+
+	// cli.Start()
 }
